a06fc0d088fec4dc9ae2f3051c0a8c5b1b5d33d6
MooseGoggles <jack.lyons@gmail.com>
2018-08-20 08:53:23 -0400

    persist font size

--- scratch.js
+++ scratch.js
@@ -10,6 +10,13 @@ if ( localStorage['scratchText'] === undefined ) {
 	t.innerHTML = localStorage['scratchText'];
 }
 
+// Load and apply the font size from localstorage if it exists
+if ( localStorage['scratchFontSize'] === undefined ) {
+	localStorage['scratchFontSize'] = 10;
+	t.style.fontSize = '10pt';
+}
+t.style.fontSize = localStorage['scratchFontSize'] + 'pt';
+
 // put the cursor at the beginning of the textarea
 t.setSelectionRange(0,0);
 t.focus();
@@ -26,12 +33,16 @@ document.addEventListener('keyup', function(){
 var b1 = document.getElementById('fontSizeUp');
 var b2 = document.getElementById('fontSizeDn');
 b1.addEventListener('click', function() {
-	var style = window.getComputedStyle(t, null).getPropertyValue('font-size');
-	var fontSize = parseInt(style);
-	t.style.fontSize = (fontSize + 2) + 'px';	
+	var fontSize = parseInt(localStorage['scratchFontSize']);
+	console.log("fontSize = " + fontSize);
+	fontSize += 2;
+	console.log("fontSize + 2 = " + fontSize);
+	localStorage['scratchFontSize'] = fontSize;
+	t.style.fontSize = fontSize + 'pt';	
 });
 b2.addEventListener('click', function() {
-	var style = window.getComputedStyle(t, null).getPropertyValue('font-size');
-	var fontSize = parseInt(style); 
-	t.style.fontSize = (fontSize - 2) + 'px';	
+	var fontSize = parseInt(localStorage['scratchFontSize']);
+	fontSize -= 2;
+	localStorage['scratchFontSize'] = fontSize;
+	t.style.fontSize = fontSize + 'pt';	
 });
